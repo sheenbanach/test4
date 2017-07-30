858168579b632aee8a1619cff26e65a6e61b2f9a
sheenbanach <daimon.fuji@gmail.com>
2017-07-30 12:30:27 +0900

    5.1
    5.1

--- db/migrate/20170730030217_add_attachment_photo_to_pictures.rb
+++ db/migrate/20170730030217_add_attachment_photo_to_pictures.rb
@@ -1,4 +1,4 @@
-class AddAttachmentPhotoToPictures < ActiveRecord::Migration
+class AddAttachmentPhotoToPictures < ActiveRecord::Migration[5.1]
   def self.up
     change_table :pictures do |t|
       t.attachment :photo
