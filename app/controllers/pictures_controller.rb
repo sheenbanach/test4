599cb5443f47acc8d637e0ba5e38db073a804135
sheenbanach <daimon.fuji@gmail.com>
2017-07-31 18:29:50 +0900

    permit
    permit

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -70,6 +70,6 @@ class PicturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def picture_params
-      params.require(:picture).permit(:name, :photo)
+      params.require(:picture).permit(:name :photo)
     end
 end
