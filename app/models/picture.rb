5b4aa5902cdbd8b1bdd6c76b30874a9f684f22b9
sheenbanach <daimon.fuji@gmail.com>
2017-08-01 12:07:14 +0900

    Update picture.rb

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,4 @@
 class Picture < ActiveRecord::Base
   has_attached_file :photo, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }
-  validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
