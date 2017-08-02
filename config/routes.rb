d3b8460a17f860b3f1922098ceb5b592ec5c80aa
sheenbanach <daimon.fuji@gmail.com>
2017-08-02 13:31:39 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get 'fish/homepage'
   get 'books/homepage'
   get 'toriki/index'
-  root 'toriki/homepage'
+  root 'toriki#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
